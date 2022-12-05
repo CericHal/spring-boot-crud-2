d5c5b22b82e51ea659af79b26bf0a4910161ac1a
CericHal <christianhalikias@gmail.com>
2022-12-05 16:17:27 -0600

    refactored code to use service instead of DAO

--- cruddemo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ cruddemo/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -9,24 +9,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
 import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-    private EmployeeDAO employeeDAO;
+    private EmployeeService employeeService;
 
     // quick and dirty: inject employee dao (use constructor injection)
     @Autowired
-    public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
-        employeeDAO = theEmployeeDAO;
+    public EmployeeRestController(EmployeeService theEmployeeService) {
+        employeeService = theEmployeeService;
     }
 
 
     // "/employees" and return list of employees
     @GetMapping("/employees")
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeService.findAll();
     }
     
 }
